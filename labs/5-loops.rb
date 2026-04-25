bca7ad7f921dd85855c4863d1454ad5491d87e2b
healthy-kraken <simon.serna@kellogg.northwestern.edu>
2026-04-25 00:06:30 +0000

    Airplane run

--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -10,14 +10,14 @@ diamonds = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 hearts = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 spades = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 
-#Step 1.
-#for ranks in ranks
-# put ranks with the suit
-#
 
-#Step 2.
-#for suit in suits
-# put rank with suit 
+suits = ["Clubs","Diamonds","Hearts","Spades"]
+
+# for suit in suits
+#   for card in clubs
+#     puts "#{card} of #{suit}"
+#   end
+# end
 
 # Sample output:
 # 2 of Clubs
@@ -32,9 +32,50 @@ spades = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 # The arrays are identical for each suit and can be simplified by using
 # a ranks array as seen below. Try to complete the exercise again by
 # combining these arrays.
-ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
-suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+# ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+# suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+# for suit in suits
+#   for rank in ranks
+#     puts "#{rank} of #{suit}"
+#   end
+# end
 
 # CHALLENGE #2
 # Deal a poker hand. Shuffle the deck and "deal" (i.e. display) a 5 card hand (i.e. 5 cards from the deck).
 # You will want to look at the documentation for Arrays: https://ruby-doc.org/core-2.7.0/Array.html
+
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+# New Deck
+new_deck = []
+hand = []
+
+#generates new deck
+for suit in suits
+  for rank in ranks
+    new_deck.push("#{rank} of #{suit}")
+  end
+end
+
+#generates cards from deck
+hand = []
+while hand.length < 5
+  hand = []
+  for i in 0...5
+    card = new_deck[rand(new_deck.length)]
+    hand.push(card)
+    puts "Card #{i+1}: #{card}"
+  end
+
+  if hand.length != hand.uniq.length
+    puts "Duplicates found, re-dealing.."
+  end
+end
+
+puts "Final Hand"
+for card in hand
+  puts card
+end
+
